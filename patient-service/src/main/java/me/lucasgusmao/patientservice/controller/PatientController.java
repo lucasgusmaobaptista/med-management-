234558583ee11d010b51303ff2be7fe638ff4ca6
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-21 14:32:22 -0300

    create getPatients method

--- patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
@@ -1,10 +1,14 @@
 package me.lucasgusmao.patientservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.patientservice.dto.PatientResponseDTO;
 import me.lucasgusmao.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -13,5 +17,9 @@ public class PatientController {
 
     private final PatientService patientService;
 
-
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok(patients);
+    }
 }
