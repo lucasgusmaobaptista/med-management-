62b3b727492b406f308ea812d08b1845090f75a2
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-21 14:15:36 -0300

    create PatientRepository

---
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package me.lucasgusmao.patientservice.repository;
+
+import me.lucasgusmao.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
