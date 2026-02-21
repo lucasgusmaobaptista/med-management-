b0044e0d498b0040bcf8a85afe35082298ba5860
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-21 14:21:04 -0300

    create PatientService and getPatients method

---
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/service/PatientService.java
@@ -0,0 +1,25 @@
+package me.lucasgusmao.patientservice.service;
+
+import lombok.RequiredArgsConstructor;
+import me.lucasgusmao.patientservice.dto.PatientResponseDTO;
+import me.lucasgusmao.patientservice.mapper.PatientMapper;
+import me.lucasgusmao.patientservice.model.Patient;
+import me.lucasgusmao.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream()
+                .map(PatientMapper::toDTO)
+                .toList();
+    }
+
+}
