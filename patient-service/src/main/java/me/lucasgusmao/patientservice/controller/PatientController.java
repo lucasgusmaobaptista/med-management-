13ddcfb7221aad441a04139294d4cd35f0d493c6
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-23 13:51:41 -0300

    add updatePatient endpoint

--- patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
@@ -1,15 +1,18 @@
 package me.lucasgusmao.patientservice.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import lombok.RequiredArgsConstructor;
 import me.lucasgusmao.patientservice.dto.PatientRequestDTO;
 import me.lucasgusmao.patientservice.dto.PatientResponseDTO;
 import me.lucasgusmao.patientservice.service.PatientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -28,5 +31,12 @@ public class PatientController {
     public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDTO createdPatient = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPatient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,
+        @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO updatedPatient = patientService.updatePatient(id, patientRequestDTO);
+        return ResponseEntity.ok(updatedPatient);
     }
 }
