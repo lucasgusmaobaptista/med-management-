6ffafd64627e9f396563b17d11fa66a7f65a9abc
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-02-24 14:38:30 -0300

    add deletePatient endpoint

--- patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/controller/PatientController.java
@@ -39,4 +39,10 @@ public class PatientController {
         PatientResponseDTO updatedPatient = patientService.updatePatient(id, patientRequestDTO);
         return ResponseEntity.ok(updatedPatient);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
