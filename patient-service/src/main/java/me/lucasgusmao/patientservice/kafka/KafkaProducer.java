b6d2d305add33b68907d3bd167b14ef5d0edfcfe
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2026-03-12 21:22:01 -0300

    create KafkaProducer

---
+++ patient-service/src/main/java/me/lucasgusmao/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,15 @@
+package me.lucasgusmao.patientservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+
+}
